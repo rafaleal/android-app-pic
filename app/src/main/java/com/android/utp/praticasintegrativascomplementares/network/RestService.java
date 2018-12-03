43792c9bf2cb6640403a0b76fccf0a38c134f288
Rafael Leal <leal.rafa22@gmail.com>
2018-12-03 16:53:19 -0200

    Add new endpoints

--- app/src/main/java/com/android/utp/praticasintegrativascomplementares/network/RestService.java
+++ app/src/main/java/com/android/utp/praticasintegrativascomplementares/network/RestService.java
@@ -17,6 +17,14 @@ public interface RestService {
     @POST("/ubs/get_ubs_por_distrito.php")
     Call<GetUBSResponse> getUbs(@PartMap Map<String, RequestBody> params);
 
+    @Multipart
+    @POST("/ubs/get_ubs_por_pic.php")
+    Call<GetUBSResponse> getUbsByPic(@PartMap Map<String, RequestBody> params);
+
     @POST("/pic/get_pics.php")
     Call<GetPICResponse> getPics();
+
+    @Multipart
+    @POST("/pic/get_pics_por_ubs.php")
+    Call<GetPICResponse> getPicsByUBS(@PartMap Map<String, RequestBody> params);
 }
